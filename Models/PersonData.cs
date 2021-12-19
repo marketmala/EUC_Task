ef14919f849089fa77d651f5e3095a9c28ccda78
Marketa_Mala <marketa.mala@tul.cz>
2021-12-19 10:57:18 +0100

    Add project files.

---
+++ Models/PersonData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EUCTask.Models
+{
+    public class PersonData : IPersonData
+
+    {
+    }
+}
